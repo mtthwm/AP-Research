1c9b29e1b98108fd2bcce1eecf04b247d88e0e61
Matthew <mtthwmrls9@gmail.com>
2021-04-05 20:05:44 -0600

    Trying multithreading. This could make it so that several images are processsed in parallel, which would merge several images into one final output file, increasing its randomness.

--- index.py
+++ index.py
@@ -10,56 +10,59 @@ from datetime import datetime
 import sys
 from time import time
 from utils.CommandLineArgs import CommandLineArgs
+from concurrent.futures import ThreadPoolExecutor
 
 def main (txt_file:str, num_blocks:int, search_term:str, retain_originals:bool, retain_finals:bool):
-    ORIGINAL_IMG_DIR = 'images/originals/'
-    FINAL_IMG_DIR = 'images/final/'
+    with ThreadPoolExecutor() as executor:
+        ORIGINAL_IMG_DIR = 'images/originals/'
+        FINAL_IMG_DIR = 'images/final/'
 
-    twitter = TwitterAPI(os.getenv('TWITTER_API_BEARER_TOKEN'))
+        twitter = TwitterAPI(os.getenv('TWITTER_API_BEARER_TOKEN'))
 
-    twitter.clear_rules()
-    twitter.add_rule('Posts with images', 'nature', has=['images'])
+        twitter.clear_rules()
+        twitter.add_rule('Posts with images', search_term, has=['images'])
 
-    tweet_stream = twitter.open_stream(expansions='attachments.media_keys', media__fields=['url'])
+        tweet_stream = twitter.open_stream(expansions='attachments.media_keys', media__fields=['url'])
 
-    image_paths = []
+        image_paths = []
 
-    total_bits_generated = 0
-    
-    start_time = time()
+        total_bits_generated = 0
+        
+        start_time = time()
 
-    for x in tweet_stream:
-        if len(image_paths) >= num_blocks:
-            break
-        try:
-            y = x['includes']['media'][0]
-            url = y['url']
-            remote_filename = url.split('/')[-1]
-            image_id = remote_filename.split('.')[0]
-            filename = os.path.join(ORIGINAL_IMG_DIR, remote_filename)
-            if os.path.exists(filename):
-                print("Image has been downloaded previously.")                
-            else:
-                local_image_name = download_url_image(url, filename)
-                total_bits_generated
-                outname =  datetime.now().strftime("%m%d%Y%H%M%S%f")+f"({image_id}).png"
-                seq = arnold_cat_map(local_image_name, os.path.join(FINAL_IMG_DIR, outname), retain_finals, txt_file)
-                total_bits_generated = total_bits_generated + len(seq)
-                image_paths.append(seq.outname)
-                if not retain_originals:
-                    os.remove(local_image_name)
-        except KeyError as e:
-            print('DOES NOT CONTAIN MEDIA')
-            pass
+        for x in tweet_stream:
+            if len(image_paths) >= num_blocks:
+                break
+            try:
+                y = x['includes']['media'][0]
+                url = y['url']
+                remote_filename = url.split('/')[-1]
+                image_id = remote_filename.split('.')[0]
+                filename = os.path.join(ORIGINAL_IMG_DIR, remote_filename)
+                if os.path.exists(filename):
+                    print("Image has been downloaded previously.")                
+                else:
+                    local_image_name = download_url_image(url, filename)
+                    total_bits_generated
+                    outname =  datetime.now().strftime("%m%d%Y%H%M%S%f")+f"({image_id}).png"
+                    f = executor.submit(arnold_cat_map, local_image_name, os.path.join(FINAL_IMG_DIR, outname), retain_finals, txt_file)
+                    seq = f.result()
+                    total_bits_generated = total_bits_generated + len(seq)
+                    image_paths.append(seq.outname)
+                    if not retain_originals:
+                        os.remove(local_image_name)
+            except KeyError as e:
+                print('DOES NOT CONTAIN MEDIA')
+                pass
 
-    time_elapsed = time() - start_time
-    print("\nGenerated: ")
-    for i in image_paths:
-        print(i)
-    print(f"({total_bits_generated} bits) in {time_elapsed} sec. {total_bits_generated / time_elapsed} bits/sec")
+        time_elapsed = time() - start_time
+        print("\nGenerated: ")
+        for i in image_paths:
+            print(i)
+        print(f"({total_bits_generated} bits) in {time_elapsed} sec. {total_bits_generated / time_elapsed} bits/sec")
 
 filename = os.path.join("data/", datetime.now().strftime("%m-%d-%Y(%H%M%S)") + ".txt")
 
-args = CommandLineArgs(sys.argv, 'file:str:!', 'num_blocks:int:!', 'search_term:str:nature', 'retain_originals:bool:False', 'retain_finals:bool:False')
+args = CommandLineArgs(sys.argv, 'file:str:!', 'num_blocks:int:!', 'search_term:str:grand prismatic spring', 'retain_originals:bool:False', 'retain_finals:bool:False')
 print(args.get('num_blocks'))
 main(filename, num_blocks=args.get('num_blocks'), search_term=args.get('search_term'), retain_originals=args.get('retain_originals'), retain_finals=args.get('retain_finals'))
