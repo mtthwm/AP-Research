185245db9c5f146f89b4edf2d977b4447a5d3c2d
Matthew <mtthwmrls9@gmail.com>
2021-04-07 00:27:33 -0600

    Did some refactoring. Also modified the program to use a hashtag instead of a search term. I've noticed the photograhy hastag seems to provide a huge volume of high resolution tweets that will increase the output and speed of the generator. I will use #photography for the tests.

--- index.py
+++ index.py
@@ -12,29 +12,40 @@ from time import time
 from utils.CommandLineArgs import CommandLineArgs
 from concurrent.futures import ThreadPoolExecutor
 
-def main (txt_file:str, sequence_length:int, search_term:str, retain_originals:bool, retain_finals:bool):
-    ORIGINAL_IMG_DIR = 'images/originals/'
-    FINAL_IMG_DIR = 'images/final/'
+txt_file = os.path.join("data/", datetime.now().strftime("%m-%d-%Y(%H%M%S)") + ".txt")
 
-    twitter = TwitterAPI(os.getenv('TWITTER_API_BEARER_TOKEN'))
+args = CommandLineArgs(sys.argv, 'file:str:!', 'sequence_length:int:!', 'retain_originals:bool:False', 'retain_finals:bool:False', 'hashtag:str:photography')
+print(args._arguments)
+sequence_length = args.get('sequence_length') 
+hashtag = args.get('hashtag')
+retain_originals = args.get('retain_originals')
+retain_finals = args.get('retain_finals')
 
-    twitter.clear_rules()
-    twitter.add_rule('Posts with images', search_term, has=['images'])
+ORIGINAL_IMG_DIR = 'images/originals/'
+FINAL_IMG_DIR = 'images/final/'
 
-    tweet_stream = twitter.open_stream(expansions='attachments.media_keys', media__fields=['url'])
+twitter = TwitterAPI(os.getenv('TWITTER_API_BEARER_TOKEN'))
 
-    image_paths = []
+twitter.clear_rules()
+twitter.add_rule('Posts with images', f'#{hashtag}', has=['images'])
 
-    total_bits_generated = 0
-    
-    start_time = time()
-           
-    executor = ThreadPoolExecutor()
-    for x in tweet_stream:
-        if total_bits_generated >= sequence_length:
-            break
-        try:
-            y = x['includes']['media'][0]
+tweet_stream = twitter.open_stream(expansions='attachments.media_keys', media__fields=['url'])
+
+image_paths = []
+
+total_bits_generated = 0
+
+start_time = time()
+        
+executor = ThreadPoolExecutor()
+
+terminate_loop = False
+
+for x in tweet_stream:
+    if terminate_loop:
+        break
+    try:
+        for y in x['includes']['media']:
             url = y['url']
             remote_filename = url.split('/')[-1]
             image_id = remote_filename.split('.')[0]
@@ -43,25 +54,21 @@ def main (txt_file:str, sequence_length:int, search_term:str, retain_originals:b
                 print("Image has been downloaded previously.")                
             else:
                 local_image_name = download_url_image(url, filename)
-                total_bits_generated
                 outname =  datetime.now().strftime("%m%d%Y%H%M%S%f")+f"({image_id}).png"
                 seq = arnold_cat_map(local_image_name, os.path.join(FINAL_IMG_DIR, outname), retain_finals, txt_file)
                 total_bits_generated = total_bits_generated + len(seq)
                 image_paths.append(seq.outname)
                 if not retain_originals:
                     os.remove(local_image_name)
-        except KeyError as e:
-            print('DOES NOT CONTAIN MEDIA')
-            pass
+            if total_bits_generated >= sequence_length:
+                terminate_loop = True
+                break
+    except KeyError as e:
+        print('DOES NOT CONTAIN MEDIA')
+        pass
 
-    time_elapsed = time() - start_time
-    print("\nGenerated: ")
-    for i in image_paths:
-        print(i)
-    print(f"({total_bits_generated} bits) in {time_elapsed} sec. {total_bits_generated / time_elapsed} bits/sec")
-
-filename = os.path.join("data/", datetime.now().strftime("%m-%d-%Y(%H%M%S)") + ".txt")
-
-args = CommandLineArgs(sys.argv, 'file:str:!', 'sequence_length:int:!', 'retain_originals:bool:False', 'retain_finals:bool:False', 'search_term:str:yellowstone')
-print(args._arguments)
-main(filename, sequence_length=args.get('sequence_length'), search_term=args.get('search_term'), retain_originals=args.get('retain_originals'), retain_finals=args.get('retain_finals'))
+time_elapsed = time() - start_time
+print("\nGenerated: ")
+for i in image_paths:
+    print(i)
+print(f"({total_bits_generated} bits) in {time_elapsed} sec. {total_bits_generated / time_elapsed} bits/sec")
